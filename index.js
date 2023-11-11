dbe71142efdc2ad517900230cdadabec8f1bed64
MarcyMarbles <80337974+MarcyMarbles@users.noreply.github.com>
2023-11-11 21:59:05 +0600

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const { TwitterVideoDownloader } = require('./twitter.js');
-const {unlink, writeFile} = require('fs');
-
-const url = 'https://twitter.com/i/status/1626759413045272577';
-
-const downloader = new TwitterVideoDownloader(url);
-downloader.Download(5000000) // Максимальный размер файла
